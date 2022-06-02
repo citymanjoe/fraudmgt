9a30e628695dce93f64840e698e99336cb0a1108
Emmanuel Njoku <Emmanuel.njoku@wemabank.com>
2022-06-02 16:46:15 +0100

    Initial commit

---
+++ src/main/java/com/telcoilng/fraudmgt/FraudmgtApplication.java
@@ -0,0 +1,13 @@
+package com.telcoilng.fraudmgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudmgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudmgtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telcoilng/fraudmgt/FraudmgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telcoilng.fraudmgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudmgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
